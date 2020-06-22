390996bc1d9bede95c63dc112e3a97438bb4db6a
Peter Robins <gitlab@peterrobins.co.uk>
2020-06-22 16:42:24 +0100

    Fix 21781 and 3912 defs

--- defs/epsg21781.js
+++ defs/epsg21781.js
@@ -1,12 +1,20 @@
 export default {
-  projName: 'somerc',
-  lat0: 0.8194740686761218,
-  long0: 0.12984522414316146,
-  k0: 1,
-  x0: 600000,
-  y0: 200000,
-  ellps: 'bessel',
-  datum_params: [Array],
-  units: 'm',
-  no_defs: true
+  "projName": "somerc",
+  "lat0": 0.8194740686761218,
+  "long0": 0.12984522414316146,
+  "k0": 1,
+  "x0": 600000,
+  "y0": 200000,
+  "ellps": "bessel",
+  "datum_params": [
+    674.374,
+    15.056,
+    405.346,
+    0,
+    0,
+    0,
+    0
+  ],
+  "units": "m",
+  "no_defs": true
 };
--- defs/epsg3912.js
+++ defs/epsg3912.js
@@ -1,12 +1,20 @@
 export default {
-  projName: 'tmerc',
-  lat0: 0,
-  long0: 0.2617993877991494,
-  k0: 0.9999,
-  x0: 500000,
-  y0: -5000000,
-  ellps: 'bessel',
-  datum_params: [Array],
-  units: 'm',
-  no_defs: true
+  "projName": "tmerc",
+  "lat0": 0,
+  "long0": 0.2617993877991494,
+  "k0": 0.9999,
+  "x0": 500000,
+  "y0": -5000000,
+  "ellps": "bessel",
+  "datum_params": [
+    577.326,
+    90.129,
+    463.919,
+    5.137,
+    1.474,
+    5.297,
+    2.4232
+  ],
+  "units": "m",
+  "no_defs": true
 };
