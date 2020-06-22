0869a9bfce12f7c1712e9979d1bb9a8a2e87e385
Peter Robins <gitlab@peterrobins.co.uk>
2020-06-22 16:12:32 +0100

    Fix 28992 def

--- defs/epsg28992.js
+++ defs/epsg28992.js
@@ -1,12 +1,20 @@
 export default {
-  projName: 'sterea',
-  lat0: 0.9102967268932393,
-  long0: 0.09403203751960008,
-  k0: 0.9999079,
-  x0: 155000,
-  y0: 463000,
-  ellps: 'bessel',
-  datum_params: [Array],
-  units: 'm',
-  no_defs: true
+  "projName": "sterea",
+  "lat0": 0.9102967268932393,
+  "long0": 0.09403203751960008,
+  "k0": 0.9999079,
+  "x0": 155000,
+  "y0": 463000,
+  "ellps": "bessel",
+  "datum_params": [
+    565.417,
+    50.3319,
+    465.552,
+    -0.398957,
+    0.343988,
+    -1.8774,
+    4.0725
+  ],
+  "units": "m",
+  "no_defs": true
 };
